88025339a18219102063bde097bb208b566f35b1
kurs <gliba@wp.pl>
2018-10-14 09:31:26 +0200

    new file

---
+++ 01_podstawy/podsumoanie_1_dnia.py
@@ -0,0 +1,36 @@
+# zmienne
+# typy
+#     int
+#     float
+#     bool
+#     str
+print("komuniat", sep="#", end="#")
+print(f'tu jest wynik obliczenia {4+3}')
+
+wynik = input('zacheca do wpisania wartosci ')
+
+#instrukcje sterujace
+if  warunek:
+    blok instrukcji jesli warunek byl prawdziwy
+
+if war1 and war2 and war3\
+        or war4 and war5\
+        or  war6 and war7:
+    blok
+    instrukcji
+    jesli warunek
+    byl
+    prawidziwy
+instrukcja za ifem
+elif war:
+    robi
+elif war:
+    robi
+
+else:
+    blok
+    instrukcji
+    jesli
+    warunek
+    nie byl
+    prawdziwy
